07650ec50792f1e4045add47f55bb01b25687a16
fengxiu <lujin133@outlook.com>
2025-03-18 10:06:28 +0800

    fix: upgrade

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -41,12 +41,34 @@ return {
         },
       },
       textobjects = {
+        select = {
+          enable = true,
+          include_surrounding_whitespace = true,
+          lookahead = true,
+          keymaps = {
+            ["ia"] = "@parameter.inner",
+            ["aa"] = "@parameter.outer",
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
         move = {
           enable = true,
           goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
           goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
           goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
           goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>a"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>A"] = "@parameter.inner",
+          },
         },
       },
     },
